55a8c569935915b50b014864ccb1846fe2a69cae
tomlord88 <tom.isokpehi@gmail.com>
2022-04-09 03:29:56 +0000

    add some html

---
+++ script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Welcome to scripting lifestyle"
+echo
+echo
+sleep 5
+echo "if you like i can create a user with a folder for you"
+sleep 2
+echo
+echo
+echo "Would you like me to go ahead and create a personal user for you"
+read ans
+#Write an if statement if the ans is yes continue if the answer id no quit
+echo "Thanks for using my services"
+echo
+echo
+echo
+sleep 3
+echo "Which one of the follwoing would you like me to do ?"
+echo "create user"
+echo "create group"
+echo "create file"
+echo "create webserver"
+read option
+sleep 3
+echo "you have selected option $option"
+echo "I will go ahead and make your wishes come through"
+#write a case state the perform any action the user picks
